4e8cc677c5b15fdc379ee2aac101f7aef686d0ff
Eric Moessner <emoessne@gmail.com>
2024-06-09 19:59:01 -0700

    2.1 — Introduction to functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
+// 2.1 — Introduction to functions
+
+
+void doPrint()
+{
+	std::cout << "In doPrint()\n";
+}
+
+void doB()
+{
+	std::cout << "In doB()\n";
+}
+
+void doA()
+{
+	std::cout << "Starting doA()\n";
+	doB();
+	std::cout << "Ending doA()\n";
+}
+
 int main()
 {
-	std::cout << "Hello World!\n";
+	std::cout << "Starting main()\n";
+	doPrint();
+	doA();
+	std::cout << "Ending main()\n";
 
 	return 0;
 }
