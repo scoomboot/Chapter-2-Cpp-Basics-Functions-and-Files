5389d8330d2f4a982d5dece91df749f280ad673e
Eric Moessner <emoessne@gmail.com>
2024-06-10 17:10:26 -0700

    2.4 — Introduction to function parameters and arguments

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 
-// 2.3 — Void functions (non-value returning functions)
+// 2.4 — Introduction to function parameters and arguments
 
-void printHi()
+/* 
+Write a complete program that reads an integer from the user,
+doubles it using the doubleNumber() function you wrote in the previous quiz question,
+and then prints the doubled value out to the console. 
+*/
+
+
+double getValue()
 {
-	std::cout << "HI" << '\n';
+	std::cout << "Enter a number: ";
+	double num{};
+	std::cin >> num;
+
+	return num;
+}
+
+void doubleNumber(double num)
+{
+	std::cout << num * 2 << '\n';
 }
 
 int main()
 {
 
-	printHi();
+	doubleNumber(getValue());
 
 	return 0;
 }
