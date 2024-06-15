1ba13e77d03574978c84279c8f20b6b3f1e8d2ec
Eric Moessner <emoessne@gmail.com>
2024-06-14 20:22:42 -0700

    2.13 — How to design your first programs

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 
 
-// 2.12 — Header guards
+// 2.13 — How to design your first programs
 
 
 int main()
 {
     std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
-
 
     return 0;
 }
