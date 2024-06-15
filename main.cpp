a1972e30d09c3409478b0b4d26e6bcacdf197c4b
Eric Moessner <emoessne@gmail.com>
2024-06-14 18:07:45 -0700

    2.11 — Header files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+#include "Add.h"
 #include <iostream>
 
-// 2.10 — Introduction to the preprocessor
+
+// 2.11 — Header files
 
 
 int main()
 {
-   
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+
+
     return 0;
 }
